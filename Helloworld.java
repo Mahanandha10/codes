eac7b8b0a73e70f11ed581e46fa7e2fe619a77d3
MICRODEN - 15 <apple@apples-iMac.local>
2024-04-01 14:07:50 +0530

    First program

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld{
+
+    void main (){
+       System.err.println("Hello, world"); 
+    }
+}
+// to enable this  javac --enable-preview --release 21 rotates.java  on complier
